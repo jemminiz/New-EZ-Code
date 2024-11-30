9f7895eb7bc08c3499e4c0bfc6577a9f04873be4
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2024-11-29 20:03:11 -0500

    Updated ports

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -10,8 +10,8 @@ namespace StratusQuo
 {
     inline ez::Drive chassis(
         // These are your drive motors, the first motor is used for sensing!
-        {1, 2, 3},     // Left Chassis Ports (negative port will reverse it!)
-        {-4, -5, -6},  // Right Chassis Ports (negative port will reverse it!)
+        {-11, -16, 13},     // Left Chassis Ports (negative port will reverse it!)
+        {17, 18, -20},  // Right Chassis Ports (negative port will reverse it!)
 
         21,      // IMU Port
         2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
