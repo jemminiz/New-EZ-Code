694f686904580b8584019d05692c370bb30766c4
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2024-12-17 08:59:23 -0500

    Forgot to change PID constants. Updated to most recent tuned.

--- src/autons.cpp
+++ src/autons.cpp
@@ -17,10 +17,10 @@ void default_constants() {
   using namespace StratusQuo;
   // P, I, D, and Start I
   // https://ez-robotics.github.io/EZ-Template/tutorials/tuning_constants
-  chassis.pid_drive_constants_set(20.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
-  chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
-  chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
+  StratusQuo::chassis.pid_heading_constants_set(11, 0, 20);
+  StratusQuo::chassis.pid_drive_constants_set(26, 0, 300);
+  StratusQuo::chassis.pid_turn_constants_set(3, 0.05, 20, 15);
+  StratusQuo::chassis.pid_swing_constants_set(6, 0, 65);
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
 
