212c1e7c366f5aad21f68eeb14802e24153049e1
Jonathan Emminizer <jonathan.emminizer@yahoo.com>
2024-12-16 20:02:30 -0500

    Initial commit for 3.2.0. Everything else should be the same, just makes it easier for me to explain things and use odom.

--- include/autons.hpp
+++ include/autons.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+void default_constants();
+
 void drive_example();
 void turn_example();
 void drive_and_turn();
@@ -8,6 +10,12 @@ void swing_example();
 void motion_chaining();
 void combining_movements();
 void interfered_example();
+void odom_drive_example();
+void odom_pure_pursuit_example();
+void odom_pure_pursuit_wait_until_example();
+void odom_boomerang_example();
+void odom_boomerang_injected_pure_pursuit_example();
+void measure_offsets();
 
 void blue_side_goal_rush();
 void red_side_goal_rush();
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 #include "main.h"
 
-#include "autons.hpp"
-#include "pros/misc.h"
-#include "subsystems.hpp"
-
-bool pto_enabled = false;
-bool limit_switch_pressed = false;
-
 /////
 // For installation, upgrading, documentations, and tutorials, check out our website!
 // https://ez-robotics.github.io/EZ-Template/
@@ -19,42 +12,52 @@ bool limit_switch_pressed = false;
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
+  using namespace StratusQuo;
+
   // Print our branding over your terminal :D
   ez::ez_template_print();
 
   pros::delay(500);  // Stop the user from doing anything while legacy ports configure
 
-  // Configure your StratusQuo::chassis controls
-  StratusQuo::chassis.opcontrol_curve_buttons_toggle(true);  // Enables modifying the controller curve with buttons on the joysticks
-  StratusQuo::chassis.opcontrol_drive_activebrake_set(0);    // Sets the active brake kP. We recommend ~2.  0 will disable.
-  StratusQuo::chassis.opcontrol_curve_default_set(0, 0);     // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
+  // Are you using tracking wheels?  Comment out which ones you're using here!
+  // chassis.odom_tracker_right_set(&right_tracker);
+  // chassis.odom_tracker_left_set(&left_tracker);
+  // chassis.odom_tracker_back_set(&horiz_tracker);  // Replace `back` to `front` if your tracker is in the front!
+
+  // Configure your chassis controls
+  chassis.opcontrol_curve_buttons_toggle(true);   // Enables modifying the controller curve with buttons on the joysticks
+  chassis.opcontrol_drive_activebrake_set(0.0);   // Sets the active brake kP. We recommend ~2.  0 will disable.
+  chassis.opcontrol_curve_default_set(0.0, 0.0);  // Defaults for curve. If using tank, only the first parameter is used. (Comment this line out if you have an SD card!)
 
   // Set the drive to your own constants from autons.cpp!
   default_constants();
 
   // These are already defaulted to these buttons, but you can change the left/right curve buttons here!
-  // StratusQuo::chassis.opcontrol_curve_buttons_left_set(pros::E_CONTROLLER_DIGITAL_LEFT, pros::E_CONTROLLER_DIGITAL_RIGHT);  // If using tank, only the left side is used.
-  // StratusQuo::chassis.opcontrol_curve_buttons_right_set(pros::E_CONTROLLER_DIGITAL_Y, pros::E_CONTROLLER_DIGITAL_A);
-  
+  // chassis.opcontrol_curve_buttons_left_set(pros::E_CONTROLLER_DIGITAL_LEFT, pros::E_CONTROLLER_DIGITAL_RIGHT);  // If using tank, only the left side is used.
+  // chassis.opcontrol_curve_buttons_right_set(pros::E_CONTROLLER_DIGITAL_Y, pros::E_CONTROLLER_DIGITAL_A);
+
   // Autonomous Selector using LLEMU
   ez::as::auton_selector.autons_add({
-      Auton("Red four ring", red_side_four_ring),
-      Auton("Blue four ring", blue_side_four_ring),
-      Auton("Blue goal rush", blue_side_goal_rush),
-      Auton("Example Drive\n\nDrive forward and come back.", drive_example),
-      Auton("Example Turn\n\nTurn 3 times.", turn_example),
-      Auton("Drive and Turn\n\nDrive forward, turn, come back. ", drive_and_turn),
-      Auton("Drive and Turn\n\nSlow down during drive.", wait_until_change_speed),
-      Auton("Swing Example\n\nSwing in an 'S' curve", swing_example),
-      Auton("Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining),
-      Auton("Combine all 3 movements", combining_movements),
-      Auton("Interference\n\nAfter driving forward, robot performs differently if interfered or not.", interfered_example),
+      {"Drive\n\nDrive forward and come back", drive_example},
+      {"Turn\n\nTurn 3 times.", turn_example},
+      {"Drive and Turn\n\nDrive forward, turn, come back", drive_and_turn},
+      {"Drive and Turn\n\nSlow down during drive", wait_until_change_speed},
+      {"Swing Turn\n\nSwing in an 'S' curve", swing_example},
+      {"Motion Chaining\n\nDrive forward, turn, and come back, but blend everything together :D", motion_chaining},
+      {"Combine all 3 movements", combining_movements},
+      {"Interference\n\nAfter driving forward, robot performs differently if interfered or not", interfered_example},
+      {"Simple Odom\n\nThis is the same as the drive example, but it uses odom instead!", odom_drive_example},
+      {"Pure Pursuit\n\nGo to (0, 30) and pass through (6, 10) on the way.  Come back to (0, 0)", odom_pure_pursuit_example},
+      {"Pure Pursuit Wait Until\n\nGo to (24, 24) but start running an intake once the robot passes (12, 24)", odom_pure_pursuit_wait_until_example},
+      {"Boomerang\n\nGo to (0, 24, 45) then come back to (0, 0, 0)", odom_boomerang_example},
+      {"Boomerang Pure Pursuit\n\nGo to (0, 24, 45) on the way to (24, 24) then come back to (0, 0, 0)", odom_boomerang_injected_pure_pursuit_example},
+      {"Measure Offsets\n\nThis will turn the robot a bunch of times and calculate your offsets for your tracking wheels.", odom_boomerang_injected_pure_pursuit_example},
   });
-  
-  // Initialize StratusQuo::chassis and auton selector
-  StratusQuo::chassis.initialize();
+
+  // Initialize chassis and auton selector
+  chassis.initialize();
   ez::as::initialize();
-  master.rumble(".");
+  master.rumble(chassis.drive_imu_calibrated() ? "." : "---");
 }
 
 /**
@@ -91,12 +94,120 @@ void competition_initialize() {
  * from where it left off.
  */
 void autonomous() {
-  StratusQuo::chassis.pid_targets_reset();                // Resets PID targets to 0
-  StratusQuo::chassis.drive_imu_reset();                  // Reset gyro position to 0
-  StratusQuo::chassis.drive_sensor_reset();               // Reset drive sensors to 0
-  StratusQuo::chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
+  using namespace StratusQuo;
+  chassis.pid_targets_reset();                // Resets PID targets to 0
+  chassis.drive_imu_reset();                  // Reset gyro position to 0
+  chassis.drive_sensor_reset();               // Reset drive sensors to 0
+  chassis.odom_xyt_set(0_in, 0_in, 0_deg);    // Set the current position, you can start at a specific position with this
+  chassis.drive_brake_set(MOTOR_BRAKE_HOLD);  // Set motors to hold.  This helps autonomous consistency
 
+  /*
+  Odometry and Pure Pursuit are not magic
+
+  It is possible to get perfectly consistent results without tracking wheels,
+  but it is also possible to have extremely inconsistent results without tracking wheels.
+  When you don't use tracking wheels, you need to:
+   - avoid wheel slip
+   - avoid wheelies
+   - avoid throwing momentum around (super harsh turns, like in the example below)
+  You can do cool curved motions, but you have to give your robot the best chance
+  to be consistent
+  */
+
+  // Uncomment this to use the auton selector
   ez::as::auton_selector.selected_auton_call();  // Calls selected auton from autonomous selector
+}
+
+/**
+ * Simplifies printing tracker values to the brain screen
+ */
+void screen_print_tracker(ez::tracking_wheel *tracker, std::string name, int line) {
+  std::string tracker_value = "", tracker_width = "";
+  // Check if the tracker exists
+  if (tracker != nullptr) {
+    tracker_value = name + " tracker: " + util::to_string_with_precision(tracker->get());             // Make text for the tracker value
+    tracker_width = "  width: " + util::to_string_with_precision(tracker->distance_to_center_get());  // Make text for the distance to center
+  }
+  ez::screen_print(tracker_value + tracker_width, line);  // Print final tracker text
+}
+
+/**
+ * Ez screen task
+ * Adding new pages here will let you view them during user control or autonomous
+ * and will help you debug problems you're having
+ */
+void ez_screen_task() {
+  using namespace StratusQuo;
+  while (true) {
+    // Only run this when not connected to a competition switch
+    if (!pros::competition::is_connected()) {
+      // Blank page for odom debugging
+      if (chassis.odom_enabled() && !chassis.pid_tuner_enabled()) {
+        // If we're on the first blank page...
+        if (ez::as::page_blank_is_on(0)) {
+          // Display X, Y, and Theta
+          ez::screen_print("x: " + util::to_string_with_precision(chassis.odom_x_get()) +
+                               "\ny: " + util::to_string_with_precision(chassis.odom_y_get()) +
+                               "\na: " + util::to_string_with_precision(chassis.odom_theta_get()),
+                           1);  // Don't override the top Page line
+
+          // Display all trackers that are being used
+          screen_print_tracker(chassis.odom_tracker_left, "l", 4);
+          screen_print_tracker(chassis.odom_tracker_right, "r", 5);
+          screen_print_tracker(chassis.odom_tracker_back, "b", 6);
+          screen_print_tracker(chassis.odom_tracker_front, "f", 7);
+        }
+      }
+    }
+
+    // Remove all blank pages when connected to a comp switch
+    else {
+      if (ez::as::page_blank_amount() > 0)
+        ez::as::page_blank_remove_all();
+    }
+
+    pros::delay(ez::util::DELAY_TIME);
+  }
+}
+pros::Task ezScreenTask(ez_screen_task);
+
+/**
+ * Gives you some extras to run in your opcontrol:
+ * - run your autonomous routine in opcontrol by pressing DOWN and B
+ *   - to prevent this from accidentally happening at a competition, this
+ *     is only enabled when you're not connected to competition control.
+ * - gives you a GUI to change your PID values live by pressing X
+ */
+void ez_template_extras() {
+  using namespace StratusQuo;
+  // Only run this when not connected to a competition switch
+  if (!pros::competition::is_connected()) {
+    // PID Tuner
+    // - after you find values that you're happy with, you'll have to set them in auton.cpp
+
+    // Enable / Disable PID Tuner
+    //  When enabled:
+    //  * use A and Y to increment / decrement the constants
+    //  * use the arrow keys to navigate the constants
+    if (master.get_digital_new_press(DIGITAL_X))
+      chassis.pid_tuner_toggle();
+
+    // Trigger the selected autonomous routine
+    if (master.get_digital(DIGITAL_B) && master.get_digital(DIGITAL_DOWN)) {
+      pros::motor_brake_mode_e_t preference = chassis.drive_brake_get();
+      autonomous();
+      chassis.drive_brake_set(preference);
+    }
+
+    // Allow PID Tuner to iterate
+    chassis.pid_tuner_iterate();
+  }
+
+  // Disable PID Tuner when connected to a comp switch
+  else {
+    if (chassis.pid_tuner_enabled())
+      chassis.pid_tuner_disable();
+  }
 }
 
 /**
@@ -113,91 +224,23 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+  using namespace StratusQuo;
   // This is preference to what you like to drive on
-  pros::motor_brake_mode_e_t driver_preference_brake = MOTOR_BRAKE_COAST;
-
-  StratusQuo::chassis.drive_brake_set(driver_preference_brake);
-
-  bool doinker_down = false;
+  chassis.drive_brake_set(MOTOR_BRAKE_COAST);
 
   while (true) {
-    // PID Tuner
-    // After you find values that you're happy with, you'll have to set them in auton.cpp
-    /*
-    if (!pros::competition::is_connected()) {
-      // Enable / Disable PID Tuner
-      //  When enabled:
-      //  * use A and Y to increment / decrement the constants
-      //  * use the arrow keys to navigate the constants
-      if (master.get_digital_new_press(DIGITAL_X))
-        StratusQuo::chassis.pid_tuner_toggle();
+    // Gives you some extras to make EZ-Template ezier
+    ez_template_extras();
 
-      // Trigger the selected autonomous routine
-      if (master.get_digital(DIGITAL_B) && master.get_digital(DIGITAL_DOWN)) {
-        autonomous();
-        StratusQuo::chassis.drive_brake_set(driver_preference_brake);
-      }
-
-      StratusQuo::chassis.pid_tuner_iterate();  // Allow PID Tuner to iterate
-    }
-    */
-    StratusQuo::chassis.opcontrol_tank();  // Tank control
-    StratusQuo::chassis.pto_toggle({StratusQuo::chassis.left_motors[2], StratusQuo::chassis.right_motors[2]}, pto_enabled);
-
-    limit_switch_pressed = StratusQuo::left_limit_switch.get_new_press()/* && StratusQuo::right_limit_switch.get_value()*/;
+    chassis.opcontrol_tank();  // Tank control
+    // chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
+    // chassis.opcontrol_arcade_standard(ez::SINGLE);  // Standard single arcade
+    // chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
+    // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
 
     // . . .
     // Put more user control code here!
     // . . .
-
-    if(pto_enabled)
-    {
-      if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L1))
-      {
-        StratusQuo::lady_brown.move(-127);
-      }
-      else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-        StratusQuo::lady_brown.move(127);
-      else
-        StratusQuo::lady_brown.brake();
-    }
-    else
-    {
-      // Drivetrain working as it should I think?
-    }
-
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN))
-    {
-      StratusQuo::lady_brown.set_pto(pto_enabled);
-      pto_enabled = !pto_enabled;
-    }
-
-    if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
-    {
-      StratusQuo::intake.move(127);
-    }
-    else if(master.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-    {
-      StratusQuo::intake.move(-127);
-    }
-    else StratusQuo::intake.brake();
-
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y))
-    {
-      StratusQuo::lady_brown.toggle();
-    }
-    if(limit_switch_pressed)
-    {
-      StratusQuo::clamp.set(true);
-    }
-
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A))
-    {
-      StratusQuo::doinker.set(!doinker_down);
-      doinker_down = !doinker_down;
-    }
-
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) StratusQuo::clamp.toggle();
 
     pros::delay(ez::util::DELAY_TIME);  // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
   }
